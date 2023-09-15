790ab8f4186754f7bc5e114580646715acb0d1a3
Lars Hagström <lars@foldspace.nu>
2023-09-15 16:00:46 +0200

    start to add better msvc detection

--- show_versions.py
+++ show_versions.py
@@ -30,25 +30,49 @@ import subprocess
 import re
 import os
 import argparse
+import platform
+import itertools
+from os.path import join, isfile, isdir
+
+if platform.system() == 'Windows':
+    try:
+        import winreg
+    except:
+        import _winreg as winreg
+    from os import environ
+else:
+    # Mock winreg and environ so the module can be imported on this platform.
+    class winreg:
+        HKEY_USERS = None
+        HKEY_CURRENT_USER = None
+        HKEY_LOCAL_MACHINE = None
+        HKEY_CLASSES_ROOT = None
+
+    environ = {}
 
 def log(*args, **kwargs):
     """Flushing log function"""
     print(*args, **kwargs)
     sys.stdout.flush()
 
+def die(message):
+    """Just raise an exception with a message"""
+    log(message)
+    sys.exit(1)
+
 def remove(path):
     """Remove a file or directory recursively"""
     if not os.path.exists(path):
         return
-    if os.path.isfile(path) or os.path.islink(path):
+    if isfile(path) or os.path.islink(path):
         os.remove(path)
         return
 
     for name in os.listdir(path):
-        if os.path.isdir(os.path.join(path, name)):
-            remove(os.path.join(path, name))
+        if isdir(join(path, name)):
+            remove(join(path, name))
         else:
-            os.remove(os.path.join(path, name))
+            os.remove(join(path, name))
     os.rmdir(path)
 
 def mkdir(newdir):
@@ -57,17 +81,152 @@ def mkdir(newdir):
         - regular file in the way, raise an exception
         - parent directory(ies) does not exist, make them as well
     """
-    if os.path.isdir(newdir):
+    if isdir(newdir):
         pass
-    elif os.path.isfile(newdir):
+    elif isfile(newdir):
         raise OSError("a file with the same name as the desired " \
                       "dir, '%s', already exists." % newdir)
     else:
         head, tail = os.path.split(newdir)
-        if head and not os.path.isdir(head):
+        if head and not isdir(head):
             mkdir(head)
         if tail:
             os.mkdir(newdir)
+
+def msvc14_find_vc2015():
+    try:
+        key = winreg.OpenKey(
+            winreg.HKEY_LOCAL_MACHINE,
+            r"Software\Microsoft\VisualStudio\SxS\VC7",
+            0,
+            winreg.KEY_READ | winreg.KEY_WOW64_32KEY
+        )
+    except OSError:
+        return None
+
+    best_version = 0
+    best_dir = None
+    with key:
+        for i in itertools.count():
+            try:
+                v, vc_dir, vt = winreg.EnumValue(key, i)
+            except OSError:
+                break
+            if v and vt == winreg.REG_SZ and isdir(vc_dir):
+                try:
+                    version = int(float(v))
+                except (ValueError, TypeError):
+                    continue
+                if version >= 14 and version > best_version:
+                    best_version, best_dir = version, vc_dir
+    return best_dir
+
+def msvc14_find_vc2017():
+    root = environ.get("ProgramFiles(x86)") or environ.get("ProgramFiles")
+    if not root:
+        return None
+
+    try:
+        path = subprocess.check_output([
+            join(root, "Microsoft Visual Studio", "Installer", "vswhere.exe"),
+            "-latest",
+            "-prerelease",
+            "-requiresAny",
+            "-requires", "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
+            "-requires", "Microsoft.VisualStudio.Workload.WDExpress",
+            "-property", "installationPath",
+            "-products", "*",
+        ]).decode(encoding="mbcs", errors="strict").strip()
+    except (subprocess.CalledProcessError, OSError, UnicodeDecodeError):
+        return None
+
+    path = join(path, "VC", "Auxiliary", "Build")
+    if isdir(path):
+        return path
+
+    return None
+
+def find_vcvarsall():
+    best_dir = msvc14_find_vc2017()
+    version = "new"
+    log("__msvc14_find_vc2017() result: " + str(best_dir))
+    if not best_dir:
+        best_dir = msvc14_find_vc2015()
+        version = "old"
+        log("__msvc14_find_vc2015() result: " + str(best_dir))
+
+    if not best_dir:
+        return None, None
+
+    vcvarsall = join(best_dir, "vcvarsall.bat")
+    if not isfile(vcvarsall):
+        return None, None
+
+    return vcvarsall,version
+
+
+def run_vcvarsall(self, vcvarsall, version):
+    if version == "old":
+        if self.arguments.use_studio not in  ("any", "vs2015"):
+            die("Could only find vs2015")
+        cmd = '"{}" {} & set'.format(vcvarsall, self.arguments.arch)
+    else:
+        if self.arguments.use_studio == "vs2015":
+            vcver = "14.0"
+        elif self.arguments.use_studio == "vs2017":
+            vcver = "14.1"
+        elif self.arguments.use_studio == "vs2019":
+            vcver = "14.2"
+        elif self.arguments.use_studio == "vs2022":
+            vcver = "14.3"
+        cmd = '"{}" {} -vcvars_ver={} & set'.format(vcvarsall, self.arguments.arch, vcver)
+
+    log("Running '" + cmd + "' to extract environment")
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    output = proc.communicate()[0]
+    if proc.returncode != 0:
+        die("Failed to fetch environment variables out of vcvarsall.bat: " + output)
+    return output
+
+def setup_build_environment():
+    """
+    Find vcvarsall.bat and load the relevant environment variables from it.  This function
+    is inspired (but not copied, for licensing reasons) by the one in python's setuptools.
+    """
+
+    vcvarsall, version = find_vcvarsall()
+
+    #use uppercase only in this variable!
+    required_variables = set(["LIB", "LIBPATH", "PATH", "INCLUDE", "VSINSTALLDIR"])
+    optional_variables = set([
+        "PLATFORM",
+    ])
+    wanted_variables = required_variables | optional_variables  #union
+
+    log("Loading Visual Studio Environment", "header")
+    output = run_vcvarsall(vcvarsall, version)
+
+    found_variables = set()
+
+    for line in output.split("\n"):
+        if '=' not in line:
+            continue
+        line = line.strip()
+        name, value = line.split('=', 1)
+        name = name.upper()
+        if name in wanted_variables:
+            if value.endswith(os.pathsep):
+                value = value[:-1]
+            if os.environ.get(name) is None:
+                log("Will set '" + name + "' to '" + value + "'", "detail")
+            else:
+                log("Will change '" + name + "' from '" + os.environ.get(name) + "' to '" + value + "'",
+                           "detail")
+            os.environ[name] = value
+            found_variables.add(name)
+
+    if len(required_variables - found_variables) != 0:
+        die("Failed to find all expected variables in vcvarsall.bat")
 
 
 def python(f):
@@ -135,6 +294,10 @@ def qt(f):
     except:
         f.write("Qt: N/A\n")
 
+def vs2015(f):
+    if platform.system() != 'Windows':
+        f.write("vs2015: N/A\n")
+        
 def msvc(f):
     olddir = os.getcwd()
     try:
@@ -192,7 +355,7 @@ def main():
                         default="versions.txt",
                         help="Name of output file")
     arguments = parser.parse_args()
-    with open(arguments.output,"w") as f:
+    with open(arguments.output,"w", encoding="utf-8") as f:
         conan(f)
         python(f)
         cmake(f)
@@ -205,7 +368,10 @@ def main():
         graphviz(f)
         qt(f)
         boost(f)
-        msvc(f)
+        vs2015(f)
+        #vs2017(f)
+        #vs2019(f)
+        #vs2022(f)
         nsis(f)
         #msvcrt
         #msvcrtd
